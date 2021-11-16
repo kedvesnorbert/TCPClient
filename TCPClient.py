14870a16f4ffda6b20224b749a9cd2e20b26c64a
kedvesnorbert <kedvesnorbert@users.noreply.github.com>
2021-11-16 19:20:46 +0200

    Sending to everyone, SEND PRIVATE message, receiving list of connected clients, switch client to send data by clicking on buttons

--- TCPClient.py
+++ TCPClient.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import sys
-import time
+from datetime import datetime
 from PyQt5 import QtWidgets
 from PyQt5 import QtGui
 from PyQt5.QtCore import *
@@ -11,9 +11,12 @@ from PyQt5.QtGui import *
 IP = "127.0.0.1"
 PORT = 27095
 FORMAT = "UTF-8"
-BUFLEN = 30
+BUFLEN = 300
 ClientSocket = 0
 MYCLIENTNAME = ""
+CLIENTS = []
+CURRENT_CLIENT = "EVERYBODY"
+CHATLOGS = []
 
 
 class ScrollLabel(QScrollArea):
@@ -28,7 +31,7 @@ class ScrollLabel(QScrollArea):
         content = QWidget(self)
         self.setWidget(content)
         # vertical box layout
-        lay = QVBoxLayout(content)
+        self.lay = QVBoxLayout(content)
 
         # creating label
         self.label = QLabel(content)
@@ -40,7 +43,7 @@ class ScrollLabel(QScrollArea):
         self.label.setWordWrap(True)
 
         # adding label to the layout
-        lay.addWidget(self.label)
+        self.lay.addWidget(self.label)
 
     # the setText method
     def setText(self, text):
@@ -51,6 +54,16 @@ class ScrollLabel(QScrollArea):
         #get text
         return self.label.text()
 
+    def addMyWidget(self, w):
+        self.lay.addWidget(w)
+        self.lay.setAlignment(Qt.AlignTop)
+
+    def removeMyWidget(self):
+        while self.lay.count():
+            item = self.lay.takeAt(0)
+            widget = item.widget()
+            widget.deleteLater()
+
 
 class LoginWindow(QWidget):
     signal_connect = pyqtSignal(bool)
@@ -58,6 +71,7 @@ class LoginWindow(QWidget):
     def openwindow(a):
         loginW.hide()
         chatW.show()
+        chatW.setWindowTitle("CHAT Client - " + str(MYCLIENTNAME))
         global receive_thread
         receive_thread = threading.Thread(target=chatW.receive_msg)
         # will be killed after main program has finished execution
@@ -71,7 +85,7 @@ class LoginWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.signal_connect.connect(self.openwindow)
-        self.setGeometry(100, 100, 900, 900)
+        self.setGeometry(50, 50, 900, 950)
         self.setWindowTitle("Login")
         self.setStyleSheet("background-color:#09325D;")
 
@@ -186,15 +200,29 @@ class LoginWindow(QWidget):
 class ChatWindow(QWidget):
     signal_recvdata = pyqtSignal(str)
     signal_senddata = pyqtSignal(bool)
+    signal_recvclientlist = pyqtSignal(str)
 
     def data_sent(self):
-        print("Data sent!")
+        print("Success!")
 
     def start_senddata_thread(self):
         t = threading.Thread(target=self.sendMSG)
         t.start()
 
     def display_msg(self, a):
+        self.a_array = a.split("\t")
+        self.tempchatlog = []
+        self.tempchatlog.append(self.a_array[1]) # 0. type
+        self.tempchatlog.append(self.a_array[2]) # 1. from_who
+        self.tempchatlog.append(self.a_array[3]) # 2. to_whom
+        self.tempchatlog.append(str(datetime.now().strftime("%H:%M"))) #3. time
+        self.tempchatlog.append(self.a_array[4]) # 4. MESSAGE
+        if self.tempchatlog[1] == MYCLIENTNAME:
+            self.tempchatlog[1] = "You"
+        global CHATLOGS
+        CHATLOGS.append(self.tempchatlog)
+        print(CHATLOGS)
+        a = self.tempchatlog[3] + " " + self.tempchatlog[1] + ":\t" + self.tempchatlog[4]
         self.getMessageLabel().setText(self.getMessageLabel().toPlainText() + a + "\n")
         self.getMessageLabel().moveCursor(QtGui.QTextCursor.End)
 
@@ -208,70 +236,151 @@ class ChatWindow(QWidget):
                 self.datalength = int(self.datalength.split("\t")[0])
                 print(self.received_data)
                 while self.receiveddata_len < self.datalength:
-                    self.received_data = self.received_data + ClientSocket.recv(BUFLEN)
+                    self.received_data = self.received_data + \
+                        ClientSocket.recv(BUFLEN)
                     self.receiveddata_len = len(self.received_data)
                     print(self.received_data)
 
                 self.received_data = self.received_data.decode(FORMAT)
                 self.datatype = int(self.received_data.split("\t")[1])
-                self.received_data = self.received_data.split("\t")[2] + "\t" + self.received_data.split("\t")[4]
-                if self.receiveddata_len == self.datalength:
-                    self.signal_recvdata.emit(self.received_data)
+                if self.datatype == 4:
+                    global CLIENTS
+                    CLIENTS = self.received_data.split("\t")[4]
+                    print("CLIENTS: " + CLIENTS)
+                    self.signal_recvclientlist.emit(CLIENTS)
                 else:
-                    self.signal_recvdata.emit(self.received_data + " MISSING DATA!!!")
+                    if self.datatype == 2:
+                        if self.receiveddata_len == self.datalength:
+                            self.signal_recvdata.emit(self.received_data)
+                        else:
+                            self.signal_recvdata.emit(self.received_data + " MISSING DATA!!!")
+                    if self.datatype == 3:
+                        if self.receiveddata_len == self.datalength:
+                            self.signal_recvdata.emit(self.received_data)
+                        else:
+                            self.signal_recvdata.emit(self.received_data + " MISSING DATA!!!")
+                    
 
             except Exception as e:
                 print(str(e))
+                if str(e)[0:16] == "[WinError 10053]":
+                    e_error = \
+                        "Megszűnt a kommunikáció a szerverrel.\nERR_CONNECTION_ABORTED\nThe estabilished connection was aborted by the client."
+                else:
+                    if str(e)[0:16] == "[WinError 10054]":
+                        e_error = \
+                            "Megszűnt a kommunikáció a szerverrel.\nA kapcsolat alaphelyzetbe állt.\nERR_CONNECTION_RESET\nThe connection was forcibly closed by the remote server."
+                    else:
+                        e_error = str(e)
                 chatW.hide()
                 loginW.show()
-                loginW.getLoginErrorMsg().setText(
-                    "Megszűnt a kommunikáció a szerverrel.\nERR_CONNECTION_RESET\nA kapcsolat alaphelyzetbe állt.")
-                break
+                loginW.getLoginErrorMsg().setText(e_error)
+                return
 
     def sendMSG(self):
         self.signal_senddata.emit(True)
-        text = self.getChatmsg().text().strip()
-        if len(text) > 0:
-            text = "2\t" + str(MYCLIENTNAME) + "\t \t" + text
-            text = text.encode(FORMAT)
-            len_text = len(text)
-            len_text_ = len(str(len_text))
-            text = text.decode(FORMAT)
-            text = str(len_text + len_text_ + 1) + '\t' + text
-            text = text.encode(FORMAT)
-            print(text)
-            self.getChatmsg().setText('')
-            try:
-                ClientSocket.send(text)
-                self.getMessageLabel().moveCursor(QtGui.QTextCursor.End)
-            except:
-                chatW.hide()
-                loginW.show()
-                loginW.getLoginErrorMsg().setText(
-                    "Megszűnt a kommunikáció a szerverrel.\nERR_CONNECTION_RESET\nA kapcsolat alaphelyzetbe állt.")
+
+        self.tempclientlist = CLIENTS.split(',')
+        self.tempclientlist.insert(0, "EVERYBODY")
+        if (CURRENT_CLIENT not in self.tempclientlist):
+            print("Message cannot be sent since the target client has disconnected!\n")
+        else:
+            text = self.getChatmsg().text().strip()
+            if len(text) > 0:
+                if str(CURRENT_CLIENT) == "EVERYBODY":
+                    text = "2\t" + str(MYCLIENTNAME) + "\t" + \
+                        str(CURRENT_CLIENT) + "\t" + text
+                else:
+                    text = "3\t" + str(MYCLIENTNAME) + "\t" + \
+                        str(CURRENT_CLIENT) + "\t" + text
+                text = text.encode(FORMAT)
+                len_text = len(text)
+                len_text_ = len(str(len_text))
+                text = text.decode(FORMAT)
+                text = str(len_text + len_text_ + 1) + '\t' + text
+                text = text.encode(FORMAT)
+                print(text)
+                self.getChatmsg().setText('')
+                try:
+                    ClientSocket.send(text)
+                    self.getMessageLabel().moveCursor(QtGui.QTextCursor.End)
+                except:
+                    chatW.hide()
+                    loginW.show()
+                    loginW.getLoginErrorMsg().setText(
+                        "Megszűnt a kommunikáció a szerverrel.\nERR_CONNECTION_RESET\nA kapcsolat alaphelyzetbe állt.")
+
+    def logout(self):
+        try:
+            ClientSocket.close()
+        except:
+            pass
 
     def __init__(self):
         super().__init__()
         self.signal_senddata.connect(self.data_sent)
-        self.setGeometry(100, 100, 900, 900)
+        self.setGeometry(50, 50, 900, 950)
         self.setWindowTitle("CHAT Client")
 
         self.layout = QHBoxLayout()
         self.layout.setAlignment(Qt.AlignTop)
 
-        self.clients_label = ScrollLabel()
-        self.clients_label.setText(
+        self.clients_label_layout = QVBoxLayout()
+        self.clients_label_layout.setAlignment(Qt.AlignTop)
+
+        self.clients_labeltitle = QLabel()
+        self.clients_labeltitle.setText(
             "<center>List of connected clients</center><br>")
+        self.clients_labeltitle.setAlignment(Qt.AlignTop)
+        self.clients_labeltitle.setFixedWidth(400)
+        self.clients_labeltitle.setFixedHeight(50)
+        self.clients_labeltitle.setStyleSheet(
+            "color:black;font-size:15pt;margin-top:5px;")
+        self.clients_label_layout.addWidget(self.clients_labeltitle)
+
+        self.clients_label = ScrollLabel()
         self.clients_label.setAlignment(Qt.AlignTop)
         self.clients_label.setFixedWidth(400)
-        self.clients_label.setStyleSheet("color:black;font-size:15pt;")
-        self.layout.addWidget(self.clients_label)
+        self.clients_label.setFixedHeight(600)
+        self.clients_label.setStyleSheet(
+            "color:black;font-size:15pt;height:50px;text-align:left;")
+        self.clients_label_layout.addWidget(self.clients_label)
 
-        self.clients_label = self.clients_label.setText(
-            self.clients_label.getText() + "\nElső\nmásodik\n...")
+        self.extrabutton_layout = QHBoxLayout()
+        self.extrabutton_layout.setAlignment(Qt.AlignBottom)
 
+        self.disconnect_btn = QPushButton("DISCONNECT")
+        self.disconnect_btn.setStyleSheet(
+            "text-align:center;font-size:12pt;color:white;height:62px;background-color:red")
+        self.disconnect_btn.clicked.connect(self.logout)
+        self.newchatroom_btn = QPushButton("NEW CHATROOM")
+        self.newchatroom_btn.setStyleSheet(
+            "text-align:center;font-size:12pt;color:black;height:62px;background-color:lightgray")
+        self.extrabutton_layout.addWidget(self.disconnect_btn)
+        self.extrabutton_layout.addWidget(self.newchatroom_btn)
+
+        self.clients_label_layout.addLayout(self.extrabutton_layout)
+
+        self.layout.addLayout(self.clients_label_layout)
+
+        #Chat layout*******************#
         self.chatlayout = QVBoxLayout()
         self.layout.addLayout(self.chatlayout)
+
+        self.chatheaderlayout = QHBoxLayout()
+        self.chatheaderinfo = QLabel()
+        self.chatheaderinfo.setStyleSheet(
+            "background-color:lightblue;font-size:14pt;font-weight:bold;text-align:left;margin-left:20px;margin-left:20px;")
+        self.chatheaderinfo.setMinimumHeight(47)
+        self.chatheaderlayout.addWidget(self.chatheaderinfo)
+
+        self.sendfile_btn = QPushButton("SEND FILE")
+        self.sendfile_btn.setStyleSheet("font-size:12pt;")
+        self.sendfile_btn.setMinimumHeight(47)
+        self.sendfile_btn.setMaximumWidth(150)
+        self.chatheaderlayout.addWidget(self.sendfile_btn)
+
+        self.chatlayout.addLayout(self.chatheaderlayout)
 
         self.message_label = QTextEdit()
         self.message_label.setReadOnly(True)
@@ -301,18 +410,88 @@ class ChatWindow(QWidget):
 
         self.sendmsg_btn = QPushButton('SEND')
         self.sendmsg_btn.setStyleSheet(
-            "font-size:12pt;color:red;height:65px;width:140px;")
+            "font-size:12pt;color:black;height:65px;width:140px;")
         self.chatsendlayout.addWidget(self.sendmsg_btn)
         self.sendmsg_btn.clicked.connect(self.start_senddata_thread)
         self.sendmsg_btn.setAutoDefault(True)
+
         self.setLayout(self.layout)
         self.signal_recvdata.connect(self.display_msg)
+        self.signal_recvclientlist.connect(self.addClientButtons)
 
     def getChatmsg(self):
         return self.chatmsg
 
     def getMessageLabel(self):
         return self.message_label
+
+    def addClientButtons(self):
+        self.clients_label.removeMyWidget()
+        global CLIENTS
+        global CURRENT_CLIENT
+        tempCLIENTS = CLIENTS
+        CLIENTS = CLIENTS.split(',')
+        CLIENTS.insert(0, 'EVERYBODY')
+        if str(MYCLIENTNAME) in CLIENTS: CLIENTS.remove(MYCLIENTNAME)
+
+        for client in CLIENTS:
+            self.clientbutton = QPushButton()
+            self.clientbutton.setText(str(client))
+            self.clientbutton.setObjectName(str(client))
+            self.buttontext = self.clientbutton.text()
+            if self.buttontext == CURRENT_CLIENT:
+                self.clientbutton.setStyleSheet(
+                    "height:50px;font-size:12pt;text-align:center;background-color:lightblue;border: 3px solid red;")
+                self.clientbutton.setDisabled(True)
+            else:
+                self.clientbutton.setStyleSheet(
+                    "height:50px;font-size:12pt;text-align:center;background-color:lightgray;")
+                self.clientbutton.setDisabled(False)
+            self.clients_label.addMyWidget(self.clientbutton)
+            self.clientbutton.clicked.connect(self.switchChatUser)
+        self.chatheaderinfo.setText(
+            str(MYCLIENTNAME) + " --> " + str(CURRENT_CLIENT))
+        if str(CURRENT_CLIENT) == "EVERYBODY":
+            self.sendfile_btn.hide()
+        else:
+            self.sendfile_btn.show()
+        CLIENTS = tempCLIENTS
+
+    def switchChatUser(self):
+        sending_button = self.sender()
+        global CURRENT_CLIENT
+        CURRENT_CLIENT = str(sending_button.objectName())
+        print(CURRENT_CLIENT)
+
+        self.clients_label.removeMyWidget()
+        global CLIENTS
+        tempCLIENTS = CLIENTS
+        CLIENTS = CLIENTS.split(',')
+        CLIENTS.insert(0, 'EVERYBODY')
+        if str(MYCLIENTNAME) in CLIENTS: CLIENTS.remove(MYCLIENTNAME)
+
+        for client in CLIENTS:
+            self.clientbutton = QPushButton()
+            self.clientbutton.setText(str(client))
+            self.clientbutton.setObjectName(str(client))
+            self.buttontext = self.clientbutton.text()
+            if self.buttontext == CURRENT_CLIENT:
+                self.clientbutton.setStyleSheet(
+                    "height:50px;font-size:12pt;text-align:center;background-color:lightblue;border: 3px solid red;")
+                self.clientbutton.setDisabled(True)
+            else:
+                self.clientbutton.setStyleSheet(
+                    "height:50px;font-size:12pt;text-align:center;background-color:lightgray;")
+                self.clientbutton.setDisabled(False)
+            self.clients_label.addMyWidget(self.clientbutton)
+            self.clientbutton.clicked.connect(self.switchChatUser)
+        self.chatheaderinfo.setText(
+            str(MYCLIENTNAME) + " --> " + str(CURRENT_CLIENT))
+        if str(CURRENT_CLIENT) == "EVERYBODY":
+            self.sendfile_btn.hide()
+        else:
+            self.sendfile_btn.show()
+        CLIENTS = tempCLIENTS
 
 
 app = QApplication(sys.argv)
